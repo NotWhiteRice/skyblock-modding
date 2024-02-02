88d82e9f6f86c2c7b2084e12364c0569700ca58e
NotWhiteRice <tristan.sunico@gmail.com>
2024-02-02 06:40:18 -0500

    Attempted to reconstruct amadornes/fg-multiproject-template

---
+++ skylib/src/main/java/net/nwr/skylib/SkyLib.java
@@ -0,0 +1,7 @@
+package skylib.src.main.java.net.nwr.skylib;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("skylib")
+public class SkyLib {
+}
---
+++ skymetrics/src/main/java/net/nwr/skymetrics/SkyMetrics.java
@@ -0,0 +1,7 @@
+package skymetrics.src.main.java.net.nwr.skymetrics;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("skymetrics")
+public class SkyMetrics {
+}
